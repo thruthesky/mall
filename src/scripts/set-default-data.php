254ec1ec9d63933633630e9069806a7bfea4eb99
JaeHo Song <thruthesky@gmail.com>
2015-06-02 21:55:44 +0900

    Setting default datas

---
+++ src/scripts/set-default-data.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\mall\Entity\Category;
+
+set_default_data();
+
+
+function set_default_data() {
+  $category = Category::create();
+  $category->set('name', 'Category A');
+  $category->set('parent_id', 0);
+}
