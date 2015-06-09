8ac1012cd303bf46314e843b81a2fcb5a23eae73
Jeff <you@example.com>
2015-06-09 09:10:28 -0400

    error text

--- src/x.php
+++ src/x.php
@@ -283,7 +283,7 @@ class x {
       case self::ERROR_BLANK_CATEGORY_NAME : $msg = "Category name cannot be blank!."; break;
       case self::ERROR_PLEASE_LOGIN_FIRST : $msg = "Please login first!."; break;
       case self::ERROR_USER_EXISTS : $msg = "The username [ #name ] already exists!."; break;
-      case self::ERROR_NOT_YOUR_ID : $msg = "The account that your are trying to edit is not yours. You are now redirected to your own account page"; break;      
+      case self::ERROR_NOT_YOUR_ID : $msg = "The account that you are trying to edit is not yours. The page that you are seeing right now is your own account page"; break;      
       default: $msg = 'Unknown'; break;
     }
     return $msg;
