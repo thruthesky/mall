d43d2c7801fddc0b108a3a6c506a42e646d81ec2
mark <mark@gmail.com>
2015-08-05 04:53:03 -0400

    member list

--- src/Controller/MallController.php
+++ src/Controller/MallController.php
@@ -3,6 +3,7 @@ namespace Drupal\mall\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\mall\Item;
 use Drupal\mall\x;
+use Drupal\library\Member;
 
 class MallController extends ControllerBase {
 	public function admin() {
@@ -21,5 +22,40 @@ class MallController extends ControllerBase {
             '#data' => $data,
         ];
     }
+	
+	public function members(){
+		$data = [];		
+	
+		$input = x::input();
+		
+		if( empty( $input['page'] ) ) $input['page'] = 1;		
+		if( empty( $input['limit'] ) ) $input['limit'] = 10;
+		
+		$page_num = $input['page'];
+		$limit = $input['limit'];		
+		if( $page_num <= 1 ) $from = 0;
+		else $from = $limit * $page_num - 1;		
+		
+		$result = db_query("SELECT count( DISTINCT( user_id ) ) FROM mall_item");
+		$total_items = array_values( $result->fetchAssoc() )[0];
+		
+		$result = db_query("SELECT DISTINCT(user_id) FROM mall_item ORDER BY created DESC LIMIT $from, $limit");
+		$rows = $result->fetchAllAssoc('user_id',\PDO::FETCH_ASSOC);
+		
+		$members = [];
+		foreach( $rows as $row ){
+			$members[ $row['user_id'] ] = Member::load( $row['user_id'] );
+		}
+		
+		$data['members'] = $members;
+		$data['items_per_page'] = $limit;
+		$data['input'] = $input;
+		$data['total_items'] = $total_items;
+
+        return [
+            '#theme' => x::getThemeName(),
+            '#data' => $data,
+        ];
+	}
 
 }
